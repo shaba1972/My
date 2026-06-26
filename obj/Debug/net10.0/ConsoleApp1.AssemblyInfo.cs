eef49d8419e9cdb51e04e06eae293410a202aebd
shaba1972 <shabadaniel1888@gmail.com>
2026-06-26 09:41:37 +0100

    new update

--- obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
+++ obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e864d21ce01b0d447fffe8a5ce522acdb3fe31c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef0982dbe01ad2eb13eb98c9c95c7672eeda7a75")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
